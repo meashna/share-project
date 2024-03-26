a3c937b358c31ea3a3d7131cc7990f995731460a
Faeza Nasrin M B <faezanasrin901@gmail.com>
2024-03-26 11:01:21 +0530

    forget commited

--- client/src/Forget/Forget.jsx
+++ client/src/Forget/Forget.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 import "./Forget.css";
+import { RiLock2Fill } from "react-icons/ri";
 
 const Forget = () => {
   return (
     <div>
-      <div className="for-head">Forget Page by anagha</div>
+    <div className="form-container">
+      <form className="form" >
+        <h2>Forget Password</h2>
+        <div className="form-control">
+          <input
+            type="password"
+            
+            placeholder="Newpassword"
+            
+          />
+           <RiLock2Fill className="reg-logo" />
+
+        
+        </div>
+        <div className="form-control">
+          <input
+            type="password"
+           
+            placeholder="reenter password"
+          />
+            <RiLock2Fill className="reg-logo" />
+
+         
+        </div>
+        <button type="submit" className="reg-btn">
+          Login
+        </button>
+        
+
+      </form>
     </div>
-  );
+  </div>
+);
 };
 
 export default Forget;
